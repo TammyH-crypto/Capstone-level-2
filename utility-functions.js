2677e77918657df03a7613724b2f7b1b7168364e
TammyH-crypto <tmhiggins1986@gmail.com>
2024-09-13 18:40:40 +0000

    👏🛋 Checkpoint ./utility-functions.js:69273156/181 ./about.html:69273156/365

--- utility-functions.js
+++ utility-functions.js
@@ -1,31 +1,37 @@
-
+//document.write
 
 function display(message) {
   document.write(message);
 }
+// is NaN
 
  if (isntNumber) {
     return userText;
   } else {
     return userNumber;
   }
-
+//inner HTML
 
 }
 function output(message) {  
     window.outputTag.innerHTML = window.outputTag.innerHTML + message;
 }
-
+//Variable
 
 function ask(myMessage) {
  var userText = prompt(myMessage);
  var userNumber = Number(userText);
  var isntNumber = isNaN(userNumber);
   
-  
+  //let function
   
   function ask(myMessage) {
  let userText = prompt(myMessage);
  let userNumber = Number(userText);
  let isntNumber = isNaN(userNumber);
   
+    
+      //Template string
+  const userName = "Tammy";
+  const myString = "Hello ${userName}! How are you?";
+}
