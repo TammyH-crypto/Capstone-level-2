d60dd4f242fca5a0fccd58f7a71c63171ce8bf72
TammyH-crypto <tmhiggins1986@gmail.com>
2024-09-13 18:29:09 +0000

    👴💼 Checkpoint ./utility-functions.js:69273156/531 ./about.html:69273156/974

---
+++ utility-functions.js
@@ -0,0 +1,31 @@
+
+
+function display(message) {
+  document.write(message);
+}
+
+ if (isntNumber) {
+    return userText;
+  } else {
+    return userNumber;
+  }
+
+
+}
+function output(message) {  
+    window.outputTag.innerHTML = window.outputTag.innerHTML + message;
+}
+
+
+function ask(myMessage) {
+ var userText = prompt(myMessage);
+ var userNumber = Number(userText);
+ var isntNumber = isNaN(userNumber);
+  
+  
+  
+  function ask(myMessage) {
+ let userText = prompt(myMessage);
+ let userNumber = Number(userText);
+ let isntNumber = isNaN(userNumber);
+  
