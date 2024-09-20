cd875c7cea575dbc08242fa8d8a7c86b191e6483
TammyH-crypto <tmhiggins1986@gmail.com>
2024-09-20 21:38:41 +0000

    🍪🍅 Checkpoint ./utility-functions2.js:69273156/5277

--- utility-functions2.js
+++ utility-functions2.js
@@ -1,10 +1,23 @@
+/************
+  
+
+//My utility function
+//function scope allows user to reuse any variable over again within the curly braces
+function temporary2 () {
+  const x = 15
+  document.write(x)
+  
+  function temporary2 () {
+  const x = 8
+  document.write(x) 
+} 
+
 //document.write = Writes text to a document that overwrites HTML content
 
 function display(Hello) {
   document.write(Hello);
 }
-// is NaN
- //Ask function that never changes
+// is NaN = Returns the value of a text to not a number
 
   function ask1(message) {
     const usertext = prompt(message);
@@ -42,19 +55,19 @@ function ask(myMessage) {
 }
          //Boolean string = only has two values true or false
 
-         Boolean ishard = true;
-         Boolean isfun = false;
+         Boolean red <= blue = true;
+         Boolean 9 < 15 = false;
 
          //Number strings can be either complex or simple.They represent numerical values
 
-         let x = 456; Integer string
-         let y = 4.56; floating string
+         let x = 456;   //Integer String
+         let y = 4.56;  //floating string
 
          //Array strings = They represent a value that cannot be changed in java but can hold more than one value
 
-        lowesList = ["wood", "nails", "glue", "dog food"];
-        position = 2;
-        document.write(lowesList.length + " ")
+      //  lowesList = ["wood", "nails", "glue", "dog food"];
+      //  position = 2;
+      //  document.write(lowesList.length + " ")
 
          //Template string = Allows single or multiple lines of text to be used in one string
 
@@ -64,13 +77,14 @@ function ask(myMessage) {
 
       //Object string = Assigns values to an object
 
-      myHouse = {Brick};
-      myHouse.color = "Red";
-      myHouse.forsale = false;
-      myHouse.rooms = 4
+      //myHouse = {Brick};
+      //myHouse.color = "Red";
+      //myHouse.forsale = false;
+      //myHouse.rooms = 4
  }   
     
     //Typeof string = Tells you what what string is being used by the variable
  
     typeof 37 === "number";
  
+******************/
