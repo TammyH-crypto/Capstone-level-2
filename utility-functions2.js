2a17c549c024f6661fa35e1a6aa99ec0e4360d04
TammyH-crypto <tmhiggins1986@gmail.com>
2024-09-25 01:00:19 +0000

    💴🌆 Checkpoint ./utility-functions2.js:69273156/5525

--- utility-functions2.js
+++ utility-functions2.js
@@ -1,81 +1,74 @@
-
-  
-
 //My utility function
 //function scope allows user to reuse any variable over again within the curly braces
 
-function temporary2 () {
-  const x = 15
-  document.write(x)
-  
-  function temporary2 () {
-  const x = 8
-  document.write(x) 
-} 
+function temporary2() {
+  const x = 15;
+  document.write(x);
+}
+function temporary1() {
+  const x = 8;
+  document.write(x);
+}
 
 //document.write = Writes text to a document that overwrites HTML content
 
 function display(Hello) {
   document.write(Hello);
 }
-// is NaN = Returns the value of a text to not a number
 
-  function ask1(message) {
-    const usertext = prompt(message);
-    const userNumber = Number(userText);
-    const isNotNumber = isNaN(usernumber);
-    userText = "nine"
-    if (isNotNumber) {
-      return userText;
-    } else {
-      return userNumber;
-    }
-    
-  } 
-//inner HTML = Creates a HTML element that displays to the browser.
 
-function output(Beautiful) {  
-    window.outputTag.innerHTML = window.outputTag.innerHTML + message;
+//Variable function = Can be redeclared within the scope.
+
+function ask2(myMessage) {
+  var userText = prompt(myMessage);
+  var userNumber = Number(userText);
+  var isntNumber = isNaN(userNumber);
 }
 
-//Variable function = Can be redeclared within the scope. 
+//let function = Cannot be redeclared within the scope
 
-function ask(myMessage) {
- var userText = prompt(myMessage);
- var userNumber = Number(userText);
- var isntNumber = isNaN(userNumber);
-  
-  
-  //let function = Cannot be redeclared within the scope
-  
- function userContent(myMessage) {
- let userText = prompt(myMessage);
- let userNumber = Number(userText);
- let isntNumber = isNaN(userNumber);
-  
+function userContent(myMessage) {
+  let userText = prompt(myMessage);
+  let userNumber = Number(userText);
+  let isntNumber = isNaN(userNumber);
 }
 
-      
-  //Template string = Allows single or multiple lines of text to be used in one string
+//Template string = Allows single or multiple lines of text to be used in one string
 
-        const userName = "Tammy";
-        const myString = "Hello ${userName}! How are you?";
-  
-  
-function ask(wordType){
-  var userAnswer = prompt("give me a " + wordType)
-  return userAnswer
-}
- 
-function game(){
- const array =["adjective","name","adjective","silly animal","verb ending in -ing", "adjective","silly noise","place in the zoo","animal","verb","funny item","verb","food", "adjective","animal","adjective","verb ending in -ing"]
- var object = {}
- for (count = 0; count < 17; count++) {
-  
-}
-}
-  
+const userName = "Tammy";
+const myString = "Hello ${userName}! How are you?";
 
+function ask(wordType) {
+  var userAnswer = prompt("give me a " + wordType);
+  return userAnswer;
+}
+
+function game() {
+  const array = [
+    "adjective",
+    "name",
+    "adjective",
+    "silly animal",
+    "verb ending in -ing",
+    "adjective",
+    "silly noise",
+    "place in the zoo",
+    "animal",
+    "verb",
+    "funny item",
+    "verb",
+    "food",
+    "adjective",
+    "animal",
+    "adjective",
+    "verb ending in -ing",
+  ];
+  var object = {};
+  for (let count = 0; count < 17; count++) { 
+    object[count]= ask(array[count])
+  }
+  display
+}
 
 //Today, I went to the zoo with my (adjective) friend, (name). We saw a (adjective) (silly animal) that was (verb ending in -ing) while wearing a tiny hat! It made a really (adjective) sound like a (silly noise).
 
